7f11704051577e86919f40b98e524df3ff26cc8b
yojhop <yojhop@126.com>
2020-04-13 19:54:40 +0800

    fix file name

--- 1tokent0/deploy.bat
+++ 1tokent0/deploy.bat
@@ -1,2 +1,2 @@
 %node ./deployment.js ${projectName} ${version} ${env(product|test|internal)} ${branch(master if empty)}%
-node ./deployment.js 1token-t0 2.2.0 product v2.2.0
+node ./deployment.js 1token-t0 2.3.0-test-09 test
