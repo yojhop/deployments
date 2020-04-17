c04ca6083359ed2869100632fa9febadd58796ab
yojhop <yojhop@126.com>
2020-04-17 19:31:40 +0800

    ignore deploy.bat

--- 1tokent0/deploy.bat
+++ 1tokent0/deploy.bat
@@ -1,2 +1,2 @@
 %node ./deployment.js ${projectName} ${version} ${env(product|test|internal)} ${branch(master if empty)}%
-node ./deployment.js 1token-t0 2.3.0-test-09 test
+node ./deployment.js 1token-t0 2.3.0-test-14 test
